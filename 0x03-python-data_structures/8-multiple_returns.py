c234b3a4845fb83f7b432bb904ddf27c1f796a37
NIGANZE Alain <niganzealain@gmail.com>
2022-09-24 13:32:56 -0700

    alain

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    my_tuple = ()
+    if len(sentence) == 0:
+        if len(sentence) == 0:
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
